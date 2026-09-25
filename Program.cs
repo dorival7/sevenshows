395c87737cc34deef2aed7d3f963a3ba7cc0936c
Dorival Araujo <dorivalaraujo008@gmail.com>
2026-09-24 23:03:47 -0300

    V15

--- Program.cs
+++ Program.cs
@@ -109,7 +109,8 @@ builder.Services.AddCors(options =>
         {
             policy
                 .WithOrigins(
-                    "http://localhost:8081"
+                    "http://localhost:8081",
+                    "http://192.168.3.138:8081"
                 )
                 .AllowAnyHeader()
                 .AllowAnyMethod();
